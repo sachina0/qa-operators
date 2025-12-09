ad5e432487b680966773e530d7660d0cab2d5c33
sachina0 <125341335+sachina0@users.noreply.github.com>
2025-12-09 08:38:42 +0545

    Initial commit

---
+++ auto.py
@@ -0,0 +1,30 @@
+name="sacina"
+print("Hello, " + name + "! Welcome to the world of Python programming.")
+
+number1 = 10
+print("The value of number1 is:", number1)
+
+final=99.3
+print(type(final))
+
+fruits = ["apple", "1", "cherry"]
+print(fruits)
+
+a=4
+b=2
+c=a-b
+print("the subtraction is:", c)
+
+# v=input("Enter a number: ")
+# z=input("Enter another number: ")
+# sum=int(v)+int(z)
+# print("The sum is:", sum)
+
+x=5
+x+=3
+print("The updated value of x is:", x)
+
+r=8
+r%=3
+print("The  result is:", r)
+
